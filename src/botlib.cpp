25f402493aa5bd35a1c390feffa8ea9d9e2b028c
shlomo-rosenheimer <shlomo.rosenheimer@gmail.com>
2023-03-06 01:15:31 +0100

    ok

--- src/botlib.cpp
+++ src/botlib.cpp
@@ -2838,7 +2838,7 @@ void Bot::updateAimDir () {
             //qqq
             //return Vector (pev->angles.x, cr::normalizeAngles (pev->angles.y + rg.get (-90.0f, 90.0f)), 0.0f).forward () * rg.get (2.0f, 4.0f);
             // qqq was 65.0
-            return Vector (pev->angles.x, cr::normalizeAngles (pev->angles.y + rg.get (-75.0f, 75.0f)), 0.0f).forward () * rg.get (0.5f, 1.5f); // was 2.0 - 4.0
+            return Vector (pev->angles.x, cr::normalizeAngles (pev->angles.y + rg.get (-90.0f, 90.0f)), 0.0f).forward () * rg.get (2.0f, 4.0f); // was 2.0 - 4.0
          }
          return nullptr;
       };
